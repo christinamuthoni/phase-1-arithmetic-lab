6701090f396a6aa6c2ce2b90735eb3f26108f165
christine <christine.muriungi@student.moringaschool.com>
2023-10-12 10:26:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
-// Write your code here
+    let num1 = 31;
+    let num2 = 2;
+     let multiply = num1 * num2;
+
+console.log(multiply)
+
+
+// function random (r){
+//     r === Math.random();
+//     if (r > 0) {
+//     return random;
+// }
+let random= (Math.floor(Math.random() * 10))
+// }   
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4 ;
+console.log(mod);
+
+let max = 20;
+let min = 5;
+Math.max(20, 5);
+console.log(Math.max);
+
